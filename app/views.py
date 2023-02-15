be4d3a6e5b5a98836c63e395449c2ec781bec470
Aamir Ashraf <aamirnasa97@gmail.com>
2023-02-15 23:22:28 +0530

    making urls

--- app/views.py
+++ app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from .models import Student
+from django.views.generic.detail import DetailView
 # Create your views here.
+#detail view gives single instance object this is most important features
+class StudentDetailview(DetailView):
+    model=Student
